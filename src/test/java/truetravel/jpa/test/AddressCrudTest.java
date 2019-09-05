f9a05d9e4a19dbfa6b0ad1a2781d19f67bc7d0ee
Darllan Costa <darllancosta7@gmail.com>
2019-09-05 17:01:16 -0300

    Versao antiga

---
+++ src/test/java/truetravel/jpa/test/AddressCrudTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package truetravel.jpa.test;
+
+import static exemplo.jpa.test.GenericTest.logger;
+
+/**
+ *
+ * @author Darllan Costa
+ */
+public class AddressCrudTest extends GenericTest {
+    
+}
