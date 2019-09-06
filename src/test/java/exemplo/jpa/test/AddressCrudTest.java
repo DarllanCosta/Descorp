cb88b3619594862f2d2c64a30e5136e38c05a597
Victor <jvictor2985@gmail.com>
2019-09-06 11:14:06 -0300

    testes de Address pegando

--- src/test/java/exemplo/jpa/test/AddressCrudTest.java
+++ src/test/java/exemplo/jpa/test/AddressCrudTest.java
@@ -7,6 +7,10 @@ package exemplo.jpa.test;
 
 import exemplo.jpa.Address;
 import static exemplo.jpa.test.GenericTest.logger;
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.CacheRetrieveMode;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
@@ -28,7 +32,50 @@ public class AddressCrudTest extends GenericTest {
      
     }
     
-     @Test
+    @Test
+    public void atualizarAddress() {
+
+        logger.info("Atualizando Address");
+        
+        String newStreet = "Rua das acacias";
+        String newPostalCode = "54280080";
+        String newState = "Paraiba";
+        
+        Address address = em.find(Address.class, 1);
+        address.setStreet(newStreet);
+        address.setPostalCode(newPostalCode);
+        address.setState(newState);
+        
+        em.flush();
+        em.clear();
+        
+         address = em.find(Address.class, 1);
+        assertEquals(newStreet, address.getStreet());
+        assertEquals(newPostalCode, address.getPostalCode());
+        assertEquals(newState, address.getState());
+
+    }
+   
+    @Test
+    public void atualizarClienteUsuarioMerge() {
+        
+        logger.info("Atualizando Address com merge");
+        Integer newNumber = 55;
+        String newNeighborhood = "Linha do Tiro";
+        Address address = em.find(Address.class, 2);
+        address.setNumber(newNumber);
+        address.setNeighborhood(newNeighborhood);
+        em.clear();
+        em.merge(address);
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("javax.persistance.cache.retrieveMode", CacheRetrieveMode.BYPASS);
+        address = em.find(Address.class, 2, properties);
+        assertEquals(newNumber, address.getNumber());
+        assertEquals(newNeighborhood, address.getNeighborhood());
+
+    }
+       
+    @Test
     public void removerAddress() {
         logger.info("Executando removerAddress()");
         Address address = em.find(Address.class, 9);
